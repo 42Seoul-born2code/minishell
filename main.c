b6a081d20e56e2b3b40cd23cf7ab8bb41cfe5f39
joonhan <joonhan@student.42seoul.kr>
2022-11-29 19:28:15 +0900

    feat: Save envp value in linkedlist #1

--- main.c
+++ main.c
@@ -1,104 +1,101 @@
 #include "minishell.h"
-#include <stdio.h>
 
+// void	sig_handler(int signo)
+// {
+// 	pid_t	pid;
+// 	int		status;
 
-void	handler(int signum)
-{
-	pid_t	pid;
-	int		status;
-
-	pid = waitpid(-1, &status, WNOHANG);
-	if (signum == SIGINT)
-	{
-		if (pid == -1)
-		{
-
-		}
-	}
-}
-
+// 	pid = waitpid(-1, &status, WNOHANG);
+// 	if (signo == SIGINT)
+// 	{
+// 		if (pid == -1)
+// 		{
+// 			ft_putstr_fd("\n", STDOUT_FILENO);
+// 			printf("minishell$");
+// 		}
+// 		else
+// 			ft_putchar_fd('\n', STDOUT_FILENO);
+// 	}
+// 	else if (signo == SIGQUIT)
+// 	{
+// 		if (pid == -1)
+// 			ft_putstr_fd("", STDOUT_FILENO);
+// 		else
+// 			ft_putstr_fd("Quit: 3\n", STDOUT_FILENO);
+// 	}
+// }
 
 // set_signal(void)
 // {
-// 	signal(SIGINT, hadler);
-// 	signal(SIGQUIT, handler);
+// 	signal(SIGINT, sig_handler);
+// 	signal(SIGQUIT, sig_handler);
 // }
 
-char	*ft_strdup(const char *s1)
+// TODO 연결 리스트를 다시 char ** 으로 만들 때 사용하기
+// char	**get_envp(char **envp)
+// {
+// 	char	**g_envp;
+// 	int		i;
+
+// 	i = 0;
+// 	while (envp[i] != NULL)
+// 	{
+// 		i++;
+// 	}
+// 	if (!(g_envp = malloc(sizeof(char *) * (i + 1))))
+// 		return (NULL);
+// 	i = -1;
+// 	while (envp[++i])
+// 		g_envp[i] = ft_strdup(envp[i]);
+// 	g_envp[i] = NULL;
+// 	return (g_envp);
+// }
+
+void	save_envp_in_list(char **envp, t_env_list *curr_env)
 {
-	char	*p;
-	int		i;
-	int		len;
-
-	if (s1 == 0)
-		return (0);
-	len = 0;
-	while (s1[len])
-		len++;
-	p = (char *)malloc(sizeof(char) * (len + 1));
-	if (p == NULL)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		p[i] = s1[i];
-		i++;
-	}
-	p[i] = '\0';
-	return (p);
-}
-
-
-char			**get_envp(char **envp)
-{
-	char	**g_envp;
-	int		i;
+	int			i;
+	char		**kv_pair;
+	t_env_node	*env_node;
 
 	i = 0;
+	curr_env->head_node = NULL;
 	while (envp[i] != NULL)
 	{
-		i++;
-		printf("%s\n",envp[i]);
+		printf("%s\n", envp[i]);
+		kv_pair = ft_split(envp[i], '=');
+		env_node = malloc(sizeof(t_env_node));
+		env_node->line = ft_strdup(envp[i]);
+		env_node->key = kv_pair[0];
+		env_node->value = kv_pair[1];
+		ft_lstadd_back(&curr_env->head_node, ft_lstnew(env_node));
+		i += 1;
 	}
-	if (!(g_envp = malloc(sizeof(char *) * (i + 1))))
-		return (NULL);
-	i = -1;
-	while (envp[++i])
-		g_envp[i] = ft_strdup(envp[i]);
-	g_envp[i] = NULL;
-	return (g_envp);
-}
-
-void	init_env(t_env *env)
-{
-	env->head_node = NULL;
-	env->node_count = 0;
 }
 
 // TODO 연결리스트 구조체 생성하기 [v]
-// TODO 환경변수를 연결리스트로 저장하기 []
-// TODO 환경변수 연결리스트 끝에 추가하기 (libft 에서 만든 함수)
-// TODO 연결리스트를 더블 포인터 형태로 만들기
+// TODO 환경변수를 연결리스트로 저장하기 [v]
+// TODO 연결리스트를 더블 포인터 형태로 만들기 []
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*input;
-	char	*prompt;
-	t_env	curr_env;
+	char		*input;
+	char		*prompt;
+	t_env_list	curr_env;
 
 	(void)envp;
 	if (argc != 1)
 		return (EXIT_FAILURE);
-	init_env(&curr_env);
-	(void)argv;
 	prompt = "./minishell$ ";
-	while (TRUE)
-	{
-		input = readline(prompt);
-		if (!input)
-			break;
-		add_history(input);
-		free(input);
-	}
+	save_envp_in_list(envp, &curr_env);
+	(void)argv;
+	// while (TRUE)
+	// {
+	// 	input = readline(prompt);
+	// 	if (!input)
+	// 		break;
+	// 	add_history(input);
+	// 	free(input);
+	// }
+	// system("leaks minishell");
 	return (0);
 }
