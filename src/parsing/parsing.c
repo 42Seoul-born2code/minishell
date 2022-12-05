4211d1ad91304a84a05ab935ef9420ca03f7f0e9
joonhan <joonhan@student.42seoul.kr>
2022-12-05 13:24:52 +0900

    feat: Add parsing files #12

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -23,3 +23,19 @@ if (node->next->type == REDIRECTION_APPEND)
 
 
 */
+
+#include "parsing.h"
+
+void	parsing(t_token *token_list)
+{
+	t_list			*curr_node;
+	t_token_node	*curr_content;
+
+	curr_node = token_list->head_node;
+	while (curr_node != NULL)
+	{
+		curr_content = (t_token_node *)curr_node->content;
+		printf("type: %d, word: %s\n", curr_content->type, curr_content->word);
+		curr_node = curr_node->next;
+	}
+}
