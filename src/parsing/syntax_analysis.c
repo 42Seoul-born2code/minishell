122a79097fd7ebfd5a37ebbbad811cd1f1923558
joonhan <joonhan@student.42seoul.kr>
2022-12-12 15:20:19 +0900

    feat: 파이프 뒤에 파이프, 리다이렉션 뒤에 operator 검사 추가 #21

--- src/parsing/syntax_analysis.c
+++ src/parsing/syntax_analysis.c
@@ -1,28 +1,31 @@
 #include "parsing.h"
 
-// 오퍼레이터가 연속으로 나온다면 TRUE, + 에러처리까지
-// t_bool	is_continuos_operator(t_list *curr_node, t_token_node *curr_token)
-// {
-// 	e_meta	type;
-// 	t_token_node	*next_token;
+t_bool	is_continuous_pipe(t_list *curr_node)
+{
+	t_list			*next_node;
+	t_token_node	*curr_token;
+	t_token_node	*next_token;
 
-// 	type = curr_token->type;
-// 	if (type == PIPE || type == REDIR_HEREDOC || type == REDIR_APPEND \
-// 		|| type == REDIR_LEFT || type == REDIR_RIGHT)
-// 	{
-// 		if (curr_node->next != NULL && )
-// 		return (TRUE);
-// 	}
-// 	else
-// 		return (FALSE);
-// }
+	next_node = curr_node->next;
+	curr_token = curr_node->content;
+	next_token = next_node->content;
+	if (curr_token->type == PIPE && next_token->type == PIPE)
+		return (TRUE);
+	return (FALSE);
+}
 
-// 1. operator 인가?
-// 	1) next 가 NULL 인가? -> TRUE -> ERROR
-	// cat |
-//	2) next 가 operator 인가? -> TRUE -> ERROR
-	// cat | >
+t_bool	is_next_token_operator(t_list *curr_node)
+{
+	t_list			*next_node;
+	t_token_node	*next_token;
 
+	next_node = curr_node->next;
+	next_token = next_node->content;
+	if (next_token->type == PIPE || next_token->type == REDIR_LEFT || next_token->type == REDIR_RIGHT || \
+		next_token->type == REDIR_HEREDOC || next_token->type == REDIR_APPEND)
+		return (TRUE);
+	return (FALSE);
+}
 
 void	syntax_analysis(t_token *token_list)
 {
@@ -40,17 +43,18 @@ void	syntax_analysis(t_token *token_list)
 			{
 				printf("다음 노드가 없네유? FAIL 드립니다.\n");
 			}
-			// 2. operator 가 연속되었을 때
+			// 2. 파이프 바로 뒤에 파이프가 왔을 때
 			// TODO 62번줄에 입력한 예외처리를 조건문으로 처리하기
-			else if (TRUE)
+			else if (is_continuous_pipe(curr_node) == TRUE)
 			{
-				printf("또 operator 네유? FAIL 드립니다.\n");
+				printf("파이프 뒤에 파이프네유? FAIL 드립니다.\n");
+			}
+			// 3. redirection 바로 뒤에 operator 가 왔는가?
+			else if (is_next_token_operator(curr_node) == TRUE)
+			{
+				printf("redirection 뒤에 operator 가 왔네유? FAIL 드립니다.\n");
 			}
 		}
-		// if (is_continuos_operator(curr_node, curr_token) != TRUE)
-		// 	curr_node = curr_node->next;
-		// else
-		// 	throw_error();
 		curr_node = curr_node->next;
 	}
 }
@@ -59,11 +63,12 @@ void	syntax_analysis(t_token *token_list)
 	1. 따옴표가 제대로 닫혔는가? (DONE)
 		- [[echo], ['hello "world"]] | cat
 
-	2. operator 바로 뒤에 operator 인가?
-		- 1) || or | | (파이프 뒤에 파이프)
-		- 2) redirection 뒤에 operator 
-		- 반례
-			- OK: ls | > outfile
-			- OK: ls | >> outfile
+	2. 파이프 바로 뒤에 파이프가 왔는가?
+		- || or | | (파이프 뒤에 파이프)
 
+	3. redirection 바로 뒤에 operator 인가?
+
+	- TEST CASE
+		- OK: ls | > outfile
+		- OK: ls | >> outfile
 */
