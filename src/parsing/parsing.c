03fa16e7b324b0a4f96cf4dc302c616571f44140
joonhan <joonhan@student.42seoul.kr>
2022-12-20 11:14:08 +0900

    refactor: Rename curr_content to curr_token

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -10,21 +10,21 @@ static void	init_parse_info(t_parse_info *parse_info)
 void	parsing(t_token *token_list)
 {
 	t_list			*curr_node;
-	t_token_node	*curr_content;
+	t_token_node	*curr_token;
 	t_parse_info	parse_info;
 
 	init_parse_info(&parse_info);
 	curr_node = token_list->head_node;
 	while (curr_node != NULL)
 	{
-		curr_content = (t_token_node *)curr_node->content;
+		curr_token = (t_token_node *)curr_node->content;
 		// 1. WORD 인 경우
-		if (curr_content->type == WORD)
+		if (curr_token->type == WORD)
 		{
 			// 1-1. redirection 뒤는 무조건 파일 이름이 온다.
 			if (parse_info.is_redirection_found == TRUE)
 			{
-				curr_content->type = FILE_NAME;
+				curr_token->type = FILE_NAME;
 				parse_info.is_redirection_found = FALSE;
 			}
 			// 1-2. command 를 처음 발견한 경우
@@ -32,24 +32,24 @@ void	parsing(t_token *token_list)
 			// CASE1. ls << eof 
 			else if (parse_info.is_command_found == FALSE && parse_info.is_heredoc_found == FALSE)
 			{
-				curr_content->type = COMMAND;
+				curr_token->type = COMMAND;
 				parse_info.is_command_found = TRUE;
 			}
 			// 1-3. HEREDOC 뒤에 오는 WORD 는 LIMITER 
 			else if (parse_info.is_heredoc_found == TRUE)
 			{
-				curr_content->type = LIMITER;
+				curr_token->type = LIMITER;
 				parse_info.is_heredoc_found = FALSE;
 			}
 			// 1-4. 그 외의 경우에는 모두 ARGUMENT 
 			else
 			{
-				curr_content->type = ARGUMENT;
+				curr_token->type = ARGUMENT;
 			}
 		}
 
 		// 2. <, >, >> 을 받았을 때
-		else if ((curr_content->type == REDIR_LEFT || curr_content->type == REDIR_RIGHT || curr_content->type == REDIR_APPEND) \
+		else if ((curr_token->type == REDIR_LEFT || curr_token->type == REDIR_RIGHT || curr_token->type == REDIR_APPEND) \
 		&& parse_info.is_redirection_found == FALSE)
 		{
 			parse_info.is_redirection_found = TRUE;
@@ -58,19 +58,19 @@ void	parsing(t_token *token_list)
 		// 3. << HEREDOC을 받았을 때
 		// CASE1. ls << eof cat
 		// CASE2. ls cat << eof
-		else if (curr_content->type == REDIR_HEREDOC)
+		else if (curr_token->type == REDIR_HEREDOC)
 		{
 			parse_info.is_heredoc_found = TRUE;
 		}
 
 		// 4. 파이프를 받았을 때, 모두 False로 초기화
-		else if (curr_content->type == PIPE)
+		else if (curr_token->type == PIPE)
 		{
 			parse_info.is_command_found = FALSE;
 			parse_info.is_redirection_found = FALSE;
 			parse_info.is_heredoc_found = FALSE;
 		}
-		printf("type: %d, word: %s\n", curr_content->type, curr_content->word);
+		printf("type: %d, word: %s\n", curr_token->type, curr_token->word);
 		curr_node = curr_node->next;
 	}
 }
