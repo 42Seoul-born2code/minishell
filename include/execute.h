c07aa52925e24b69ecc75ced6a58d729a7356ca6
joonhan <joonhan@student.42seoul.kr>
2022-12-21 14:31:19 +0900

    feat: Add e_command_type #33
    simple command, multi command

--- include/execute.h
+++ include/execute.h
@@ -12,6 +12,13 @@
 
 # define PROMPT "./minishell$ "
 
+# define CHILD_PROCESS	0
+
+typedef enum {
+	SIMPLE_COMMAND,
+	MULTI_COMMAND,
+}	e_command_type;
+
 void	execute_minishell(t_env_list env);
 void	execute_command(t_token *token_list, t_env_list env_list);
 
