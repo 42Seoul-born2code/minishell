8481f379170b108185091f5991040795c8ee2dd1
joonhyuk.han <virq@naver.com>
2022-12-27 16:32:46 +0900

    chore: 주석 추가

--- src/execute/execute_command.c
+++ src/execute/execute_command.c
@@ -30,3 +30,25 @@ void	execute_command(t_token *token_list, t_env_list *env_list)
 		// execute_multi_command(token_list, env_list);
 	}
 }
+
+/*
+
+	1. Readline 에서 스페이스만 들어오는 경우
+	2. echo 'hello 
+		- 문법 오류
+		- 메모리 누수
+	3. heredoc
+		- ctrl + c (SIGINT): 다시 미니쉘로 돌아와야함 (히어독을 포크로 받으면 됨)
+		- ctrl + c (SIGINT) 입력 후 명령어가 실행되면 안됨.
+	4. 파이프 닫아주기
+		- cat|cat|cat ... lsof | grep minishell 로 확인 가능
+	5. ls > a | cat a
+		- 클러스터 맥에서 확인해봐야함
+	6. echo hi > a | cat a
+	7. 파이프 뒤에 히어독
+	8. exit code
+		- 시그널 처리할 때도 exit code 가 달라짐
+		- Child 에서 exit 을 받으면 128 + 시그널 번호 가 반환되어야 함 (cat 으로 입력 받고 있을 때)
+	9. PATH 를 unset 하고 ls 를 실행하면 에러 메세지가 떠야함
+	
+*/
--- src/execute/process_builtin_function.c
+++ src/execute/process_builtin_function.c
@@ -47,7 +47,8 @@ void	process_builtin_function(t_token *token_list, t_env_list *env_list, e_comma
 	t_list			*curr_node;
 	t_token_node	*curr_token;
 
-	printf("BUILTIN FUCNTION\n");
+	(void)file;
+	// printf("BUILTIN FUCNTION\n");
 	curr_node = token_list->head_node;
 	while (curr_node != NULL)
 	{
@@ -61,6 +62,7 @@ void	process_builtin_function(t_token *token_list, t_env_list *env_list, e_comma
 		}
 		else if (is_redirection(curr_token) == TRUE)
 		{
+			// TODO: echo hello > outfile 하면 dup2 를 원래 fd 로 바꿔줘야함
 			file = process_redirection(curr_node);
 		}
 		curr_node = curr_node->next;
