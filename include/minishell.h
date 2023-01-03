a66be12ea6d07d7d41ec5c3682a433b7e98749d2
joonhyuk.han <virq@naver.com>
2023-01-04 07:01:22 +0900

    docs: Remove comments

--- include/minishell.h
+++ include/minishell.h
@@ -1,23 +1,9 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// /*	CUSTOM HEADERS		*/
-// # include "libft.h"
-// # include "tokenize.h"
-// # include "execute.h"
-// # include "parsing.h"
-// # include "utils.h"
-
-// /*	BUILTIN HEADERS		*/
-// # include <signal.h>
-// # include <stdio.h>
-// # include <unistd.h>
-// # include <stdlib.h>
-// # include <string.h>
-
 # define ERROR	-1
 
-# define INVALID_ARGUMENT	"❌Error: Invalid arguments"
+# define INVALID_ARGUMENT	"❌Error: Invalid arguments: "
 
 extern int	g_exit_code;
 
