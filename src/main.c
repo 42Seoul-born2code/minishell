15df63b0df72308b9f71f5399d0eabcff78e5618
joonhyuk.han <virq@naver.com>
2023-01-04 06:54:28 +0900

    style: Add newilne

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 	argv				- The actual arguments
 	envp				- The environment variables when executing minishell
 */
-
 int	g_exit_code = EXIT_SUCCESS;
 
 int	main(int argc, char **argv, char **envp)
@@ -20,13 +19,6 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 1)
 		return (EXIT_FAILURE);
 	save_envp_in_env_list(envp, &env_list);
-	// printf("1st env_list\n");
-	// print_env_list(&env_list);
 	execute_minishell(&env_list);
 	return (0);
 }
-/*
-
-	minishell 실행 후 minishell 실행 시 ctrl \ 누르면 Quit 3 출력되는 문제
-
-*/
