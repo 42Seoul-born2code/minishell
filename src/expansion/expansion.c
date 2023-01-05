add835f2aac8f93c2e54660ee5262a29e878e498
joonhyuk.han <virq@naver.com>
2023-01-05 13:36:14 +0900

    refactor: expansion 함수의 서브 함수 생성 - save_word_and_expanded_variable - add_back_to_word_list

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -100,7 +100,7 @@ static char	*remove_whitespace(char *str)
 }
 
 static void	expand_env_variable(t_token_node *token, int *idx, \
-					t_word_list *word_list, t_quote quote_type, t_env_list *env_list)
+					t_word_list *word_list, t_env_list *env_list, t_quote quote_type)
 {
 	int		start;
 	int		word_length;
@@ -198,10 +198,64 @@ void	merge_replaced_word(t_word_list *word_list, char **replaced_word)
 	}
 }
 
-// 환경 변수를 확장해서 기존의 토큰을 교체하는 함수
-void	expansion(t_token *token_list, t_env_list *env_list)
+/*
+	1. $ 만난 경우: 환경변수 확장해서 저장
+	2. $ 만나기 전의 경우: 환경변수 만나기 전까지 문자열 저장
+*/
+void	add_back_to_word_list(t_token_node *curr_token, int *idx, t_word_list *word_list, t_env_list *env_list, t_quote quote_type)
+{
+	if (curr_token->word[*idx] == '$')
+	{
+		expand_env_variable(curr_token, idx, word_list, env_list, quote_type);
+	}
+	else
+	{
+		save_before_env_variable(curr_token->word, idx, word_list, quote_type);
+	}
+}
+
+t_word_list	*save_word_and_expanded_variable(t_token_node *curr_token, t_env_list *env_list)
 {
 	int				idx;
+	t_word_list		*word_list;
+
+	idx = 0;
+	word_list = malloc(sizeof(t_word_list));
+	word_list->head_node = NULL;
+	while (curr_token->word[idx] != '\0')
+	{
+		if (curr_token->word[idx] == '\'')
+			save_single_quoted_word(curr_token->word, &idx, word_list);
+		else if (curr_token->word[idx] == '\"')
+		{
+			ft_lstadd_back(&word_list->head_node, ft_lstnew(ft_strdup("\"")));
+			idx += 1;
+			while (curr_token->word[idx] != '\0' && curr_token->word[idx] != '\"')
+				add_back_to_word_list(curr_token, &idx, word_list, env_list, QUOTED);
+			if (curr_token->word[idx] == '\"')
+			{
+				ft_lstadd_back(&word_list->head_node, ft_lstnew(ft_strdup("\"")));
+				idx += 1;
+			}
+		}
+		else
+		{
+			while (curr_token->word[idx] != '\0' && curr_token->word[idx] != '\"' && curr_token->word[idx] != '\'')
+				add_back_to_word_list(curr_token, &idx, word_list, env_list, NOT_QUOTED);
+		}
+	}
+	return (word_list);
+}
+
+/*
+	환경 변수를 확장해서 기존의 토큰을 교체하는 함수
+
+	LINE	: 1. 작은 따옴표 안에 있는 문자열
+	LINE	: 2. 큰 따옴표 안에 있는 문자열
+	LINE	: 3. 따옴표에 둘러 쌓이지 않은 문자열
+*/
+void	expansion(t_token *token_list, t_env_list *env_list)
+{
 	char			*replaced_word;
 	t_list			*curr_node;
 	t_token_node	*curr_token;
@@ -211,63 +265,8 @@ void	expansion(t_token *token_list, t_env_list *env_list)
 	while (curr_node != NULL)
 	{
 		replaced_word = NULL;
-		word_list = malloc(sizeof(t_word_list));
-		word_list->head_node = NULL;
-		idx = 0;
 		curr_token = curr_node->content;
-		// 현재 토큰에 담긴 문자열의 처음부터 끝까지 순회
-		while (curr_token->word[idx] != '\0')
-		{
-			// 1. 작은 따옴표 안에 있는 문자열
-			if (curr_token->word[idx] == '\'')
-			{
-				save_single_quoted_word(curr_token->word, &idx, word_list);
-			}
-			// 2. 큰 따옴표 안에 있는 문자열
-			else if (curr_token->word[idx] == '\"')
-			{
-				ft_lstadd_back(
-					&word_list->head_node, ft_lstnew(ft_strdup("\"")));
-				idx += 1;
-				// 치환이 이루어지는 과정
-				while (curr_token->word[idx] != '\0' && curr_token->word[idx] != '\"')
-				{
-					// $ 만난 경우
-					if (curr_token->word[idx] == '$')
-					{
-						expand_env_variable(curr_token, &idx, word_list, QUOTED, env_list);
-					}
-					// $ 만나기 전의 경우
-					else
-					{
-						save_before_env_variable(curr_token->word, &idx, word_list, QUOTED);
-					}
-				}
-				if (curr_token->word[idx] == '\"')
-				{
-					ft_lstadd_back(&word_list->head_node, ft_lstnew(ft_strdup("\"")));
-					idx += 1;
-				}
-			}
-			// 3. 따옴표에 둘러 쌓이지 않은 문자열
-			else
-			{
-				while (curr_token->word[idx] != '\0' && curr_token->word[idx] != '\"' && curr_token->word[idx] != '\'')
-				{
-					// $ 만난 경우
-					// 띄어쓰기 제거, 살릴 quote는 살리기
-					if (curr_token->word[idx] == '$')
-					{
-						expand_env_variable(curr_token, &idx, word_list, NOT_QUOTED, env_list);
-					}
-					// $ 만나기 전의 경우
-					else
-					{
-						save_before_env_variable(curr_token->word, &idx, word_list, NOT_QUOTED);
-					}
-				}
-			}
-		}
+		word_list = save_word_and_expanded_variable(curr_token, env_list);
 		// 연결 리스트 노드들 하나로 합치기
 		merge_replaced_word(word_list, &replaced_word);
 		// 기존에 있던 word 를 교체
